6ed8e521a940605cfb2970ac6a12919488c1c767
Luca Morelli <luca.morelli.2022@uni.strath.ac.uk>
2026-01-31 11:58:24 +0000

    Created appearance parameter + created mappings of samples to crystalline/amorphous

--- sample.py
+++ sample.py
@@ -108,11 +108,156 @@ plate4 = {
     },
 }
 
+
+plate2_crystalline_wells = [
+    "A3",
+    "A10",
+    "A11",
+    "A12",
+    "B1",
+    "B4",
+    "B5",
+    "B6",
+    "B7",
+    "B9",
+    "B10",
+    "B11",
+    "C4",
+    "C5",
+    "C6",
+    "C7",
+    "C9",
+    "C10",
+    "C11",
+    "C12",
+    "D4",
+    "D5",
+    "D6",
+    "D10",
+    "D12",
+    "E9",
+    "E10",
+    "E11",
+    "E12",
+]
+
+plate3_crystalline_wells = [
+    "A2",
+    "A3",
+    "A4",
+    "A6",
+    "A7",
+    "A8",
+    "A9",
+    "A10",
+    "A11",
+    "A12",
+    "B1",
+    "B2",
+    "B3",
+    "B5",
+    "B6",
+    "B7",
+    "B8",
+    "B9",
+    "B10",
+    "B11",
+    "B12",
+    "C1",
+    "C3",
+    "C6",
+    "C7",
+    "C8",
+    "C9",
+    "C10",
+    "C11",
+    "C12",
+    "D1",
+    "D3",
+    "D4",
+    "D6",
+    "D7",
+    "D8",
+    "D9",
+    "D10",
+    "D11",
+    "D12",
+    "E1",
+    "E2",
+    "E3",
+    "E7",
+    "E8",
+    "E9",
+    "E10",
+    "E12",
+    "F3",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "G10",
+    "G11",
+]
+
+plate4_crystalline_wells = [
+    "A1",
+    "A4",
+    "A5",
+    "A6",
+    "A8",
+    "A10",
+    "B1",
+    "B2",
+    "B3",
+    "B5",
+    "B6",
+    "B8",
+    "B9",
+    "B10",
+    "B12",
+    "C1",
+    "C2",
+    "C4",
+    "C5",
+    "C6",
+    "C8",
+    "C10",
+    "C12",
+    "D2",
+    "D6",
+    "D7",
+    "D12",
+    "E2",
+    "E4",
+    "E5",
+    "E6",
+    "E7",
+    "E8",
+    "E10",
+    "F4",
+    "F5",
+    "F6",
+    "F9",
+    "F10",
+    "F12",
+    "G1",
+    "G4",
+    "G5",
+    "G6",
+    "G9",
+    "G10",
+    "G11",
+]
+
+
 class Sample:
 
     def __init__(self, filename: str, spectrum):
 
-        self.position = "Centre"
+        self.appearance = "amorphous"
+        self.position = "centre"
         self.drug = "PCM"
         self.well = "N/A"
         self.polymer = "N/A"
@@ -127,9 +272,7 @@ class Sample:
         # from rp.load.labspec
         self.spectrum = spectrum
 
-        file = (
-            filename.removeprefix(f"{ANALYSIS_FOLDER}/").removesuffix(".txt")
-        )
+        file = filename.removeprefix(f"{ANALYSIS_FOLDER}/").removesuffix(".txt")
 
         if file == "glass_reference":
             self.drug = "N/A"
@@ -154,7 +297,7 @@ class Sample:
                 c = part.removesuffix("mgml")
                 c = int(c)
                 self.concentration = c
-            
+
             if part == "edge":
                 self.position = "Edge"
 
@@ -170,19 +313,25 @@ class Sample:
                 self.polymer = plate2["polymer"][self.col]
                 self.drug_loading = plate2["drug_loading"][self.row]
                 self.polymer_loading = 100 - self.drug_loading
+                if self.well in plate2_crystalline_wells:
+                    self.appearance = "crystalline"
 
             case 3:
                 self.polymer = plate3["polymer"][self.col]
                 self.drug_loading = plate3["drug_loading"][self.row]
                 self.polymer_loading = 100 - self.drug_loading
+                if self.well in plate3_crystalline_wells:
+                    self.appearance = "crystalline"
 
             case 4:
                 self.polymer = plate4["polymer"][self.col]
                 self.drug_loading = plate4["drug_loading"][self.row]
                 self.polymer_loading = 100 - self.drug_loading
+                if self.well in plate4_crystalline_wells:
+                    self.appearance = "crystalline"
 
     def __str__(self):
         if self.well == "Glass":
             return "Glass reference"
         else:
-            return f"Sample {self.well} ({self.position.lower()}) on plate #{self.plate} at concentration {self.concentration} mg/mL {self.drug}/{self.polymer} {self.drug_loading}%/{self.polymer_loading}% "
+            return f"Sample {self.well} ({self.position}) on plate #{self.plate} at concentration {self.concentration} mg/mL {self.drug}/{self.polymer} {self.drug_loading}%/{self.polymer_loading}% {self.appearance}"
