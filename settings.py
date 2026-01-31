8b944f4d49aa65b2d71aaa9ee668407cbbb5f5aa
Luca Morelli <luca.morelli.2022@uni.strath.ac.uk>
2026-01-31 20:43:42 +0000

    Added pca and preprocessing toggles

--- settings.py
+++ settings.py
@@ -5,13 +5,17 @@ load_dotenv()
 
 ANALYSIS_FOLDER = getenv("ANALYSIS_FOLDER")
 MACBOOK_URL = getenv("MACBOOK_URL")
-OUTPUT_FOLDER = getenv("OUTPUT_FOLDER")
+PCA_OUTPUT = getenv("PCA_OUTPUT")
+SPECTRA_OUTPUT = getenv("SPECTRA_OUTPUT")
 
-# Choose parameter to colour by
-COLORBY = "drug_loading"
+# Filter samples by rows (e.g. "B")
+ROWS_TO_REMOVE = []
 
 # Filter samples by columns (e.g. 5)
 COLS_TO_REMOVE = []
+
+# Toggle whether to conduct PCA
+CONDUCT_PCA = False
 
 # % of data to remove per round of PCA (must be an integer between 0 and 100)
 CROSS_VAL = 0
@@ -28,35 +32,39 @@ DISPLAY_PCs_R2X = False
 # Toggle whether to display score scatter
 DISPLAY_SCORE_SCATTER = False
 
+# Choose parameter to colour by
+COLORBY = "concentration"
+
 # Toggle whether to display spectra
 DISPLAY_SPECTRA = False
-
-# Principle Components that you would like to plot - FIRST_PC on the x-axis
-FIRST_PC = 1
-
-# Name of plots and files
-NAME = "plates 2 & 3"
-
-# Number of Principle Components
-NUM_PCS = 3
-
-# Filter samples by rows (e.g. "B")
-ROWS_TO_REMOVE = []
 
 # Toggle whether to display sample labels
 SAMPLE_LABELS = False
 
-# Savitzky-Golay filter parameters
-SAVGOL = False
+# Number of Principle Components
+NUM_PCS = 3
 
+# Principle Components that you would like to plot - FIRST_PC on the x-axis
+FIRST_PC = 1
+
+# Principle Components that you would like to plot - SECOND_PC on the y-axis
+SECOND_PC = 2
+
+# Name of plots and files
+NAME = ""
+
+# Toggle preprocessing with Savitzky-Golay filter
+PREPROCESS_WITH_SAVGOL = False
+
+# Savitzky-Golay filter parameters
 SAVGOL_DERIVATIVE = 0
 
 SAVGOL_POLYNOMIAL = 3
 
 SAVGOL_WINDOW = 7
 
-# Principle Components that you would like to plot - SECOND_PC on the y-axis
-SECOND_PC = 2
+# Toggle preprocessing with Standard Normal Variate
+PREPROCESS_WITH_SNV = False
 
 # Crop spectra => to not crop the spectrum: (None, None)
 WAVENUMBER_RANGE = (None, None)
