b9191a60a95fbc8b27b7fb5f299ea4df007fbfea
Luca Morelli <luca.morelli.2022@uni.strath.ac.uk>
2026-01-31 20:52:53 +0000

    Combined plate 4 into dataframe for PCA

--- pca.py
+++ pca.py
@@ -13,6 +13,7 @@ from sample import Sample
 
 from settings import (
     COLS_TO_REMOVE,
+    CONDUCT_PCA,
     CROSS_VAL,
     MACBOOK_URL,
     NUM_PCS,
@@ -34,7 +35,8 @@ parsed_files, _ = parse()
 with open(f"{PCA_OUTPUT}/parsed_files.txt", "w") as f:
     json.dump(parsed_files, f)
 
-dataframes = []
+dataframes_2_and_3 = []
+dataframe_4 = []
 dicts = []
 
 for file in parsed_files:
@@ -76,7 +78,9 @@ for file in parsed_files:
 
     with open(f"{MACBOOK_URL}{file}") as f:
 
-        l = []
+        l_2_3 = []
+        l_4 = []
+
         for line in f:
             if line[0] != "#":
                 shift, intensity = line.split("\t", maxsplit=1)
@@ -93,20 +97,64 @@ for file in parsed_files:
                     if shift > WAVENUMBER_RANGE[1]:
                         continue
 
-                l.append(
-                    dict(
-                        sample=sample,
-                        shift=shift,
-                        intensity=intensity,
+                if sample.plate in [2, 3]:
+
+                    l_2_3.append(
+                        dict(
+                            sample=sample,
+                            shift=shift,
+                            intensity=intensity,
+                        )
                     )
-                )
 
-        df = pd.DataFrame(l)
-        df = df.pivot(index="sample", columns="shift", values="intensity")
-        dataframes.append(df)
+                elif sample.plate == 4:
 
+                    l_4.append(
+                        dict(
+                            sample=sample,
+                            shift=shift,
+                            intensity=intensity,
+                        )
+                    )
 
-spectral_df = pd.concat(dataframes)
+        if l_2_3 != []:
+            df = pd.DataFrame(l_2_3)
+            df = df.pivot(index="sample", columns="shift", values="intensity")
+            dataframes_2_and_3.append(df)
+
+        if l_4 != []:
+            df = pd.DataFrame(l_4)
+            df = df.pivot(index="sample", columns="shift", values="intensity")
+            dataframe_4.append(df)
+
+plates_2_and_3 = pd.concat(dataframes_2_and_3)
+
+# NOTE: Plates 2 and 3 were collected with an accumulation time of 5s whereas plate 4 was collected with an accumulation time of 3s.
+# This means that the shift values for plate 4 are different to those of plates 2 and 3. Because plate 4's shifts only vary from plates
+# 2 and 3 by a maximum of 0.07 across the entire wavenumber range (please run 'python check.py' to confirm), the decision was made to
+# combine plate 4's data into the dataframe for PCA using the shift numbers from plates 2 and 3. This means that the shift numbers for
+# plate 4 have been disregarded. To avoid this error, accumulation time should be consistent across plates.
+
+if dataframe_4 != []:
+
+    plate_4 = pd.concat(dataframe_4)
+
+    if plate_4.shape[1] != plates_2_and_3.shape[1]:
+        sys.exit(
+            "The dataframe for plate 4 does not have the same number of columns as the dataframe for plates 2 and 3."
+        )
+
+    plate_4_aligned = pd.DataFrame(
+        plate_4.values,
+        columns=plates_2_and_3.columns,
+        index=plate_4.index,
+    )
+
+    spectral_df = pd.concat([plates_2_and_3, plate_4_aligned], axis=0)
+
+else:
+    spectral_df = plates_2_and_3
+
 
 # =============
 # Preprocessing
@@ -118,7 +166,9 @@ if PREPROCESS_WITH_SAVGOL is True:
     spectral_df, _ = phi.spectra_savgol(
         SAVGOL_WINDOW, SAVGOL_DERIVATIVE, SAVGOL_POLYNOMIAL, spectral_df
     )
-    print(f"Savitzky-Golay filter applied with derivative order {SAVGOL_DERIVATIVE}, polynomial order {SAVGOL_POLYNOMIAL}, and window size {SAVGOL_WINDOW}.")
+    print(
+        f"Savitzky-Golay filter applied with derivative order {SAVGOL_DERIVATIVE}, polynomial order {SAVGOL_POLYNOMIAL}, and window size {SAVGOL_WINDOW}."
+    )
     print()
 
 
@@ -128,7 +178,7 @@ if PREPROCESS_WITH_SNV is True:
     print()
 
 
-sample_df = pd.DataFrame(dicts)
+sample_df = pd.DataFrame(dicts).set_index("sample")
 
 with open(f"{PCA_OUTPUT}/dataframes.txt", "w") as f:
     with pd.option_context(
@@ -152,46 +202,43 @@ spectral_df.to_pickle(f"{PCA_OUTPUT}/spectral_df_not_viewable.pkl")
 # Save sample_df to txt file (binary format)
 sample_df.to_pickle(f"{PCA_OUTPUT}/sample_df_not_viewable.pkl")
 
+if CONDUCT_PCA is True:
 
-if CROSS_VAL > 100 or CROSS_VAL < 0:
-    sys.exit("CROSS_VAL must be an integer between 0 and 100")
+    if CROSS_VAL > 100 or CROSS_VAL < 0:
+        sys.exit("CROSS_VAL must be an integer between 0 and 100")
 
-if NUM_PCS < 0:
-    sys.exit("NUM_PCS must be an integer greater than 0")
+    if NUM_PCS < 0:
+        sys.exit("NUM_PCS must be an integer greater than 0")
 
+    with open(f"{PCA_OUTPUT}/pca_terminal_output.txt", "w") as f:
+        with redirect_stderr(f), redirect_stdout(f):
 
-with open(f"{PCA_OUTPUT}/pca_terminal_output.txt", "w") as f:
-    with redirect_stderr(f), redirect_stdout(f):
+            # ============================
+            # Principle Component Analysis
+            # ============================
 
-        # ============================
-        # Principle Component Analysis
-        # ============================
+            pcaobj = phi.pca(spectral_df, int(NUM_PCS), cross_val=int(CROSS_VAL))
 
-        pcaobj = phi.pca(spectral_df, int(NUM_PCS), cross_val=int(CROSS_VAL))
+    with open(f"{PCA_OUTPUT}/pcaobj_{NUM_PCS} PCs_crossval{CROSS_VAL}.txt", "w") as f:
+        print(f"Principle Components: {NUM_PCS} PCs", file=f)
+        print(f"Cross_val: {CROSS_VAL}%", file=f)
+        print(f"Savitzky-Golay: {PREPROCESS_WITH_SAVGOL}", file=f)
+        print(f"Standard Normal Variate: {PREPROCESS_WITH_SNV}", file=f)
+        pprint(pcaobj, stream=f)
 
-with open(f"{PCA_OUTPUT}/pcaobj_{NUM_PCS} PCs_crossval{CROSS_VAL}.txt", "w") as f:
-                print(f"Principle Components: {NUM_PCS} PCs", file=f)
-                print(f"Cross_val: {CROSS_VAL}%", file=f)
-                print(f"Savitzky-Golay: {PREPROCESS_WITH_SAVGOL}", file=f)
-                print(f"Standard Normal Variate: {PREPROCESS_WITH_SNV}", file=f)
-                pprint(pcaobj, stream=f)
+    # Confirm PCA ran successfully
+    for i in pcaobj["r2x"]:
 
-# Confirm PCA ran successfully
-for i in pcaobj["r2x"]:
+        if np.isnan(i):
+            sys.exit(f"PCA not successful.")
 
-    if np.isnan(i):
-        sys.exit(
-            f'PCA not successful.'
-        )
+    # If successful, save pcaobj to npy file
+    np.save(f"{PCA_OUTPUT}/pcaobj_not_viewable.npy", pcaobj)
 
-# If successful, save pcaobj to npy file
-np.save(f"{PCA_OUTPUT}/pcaobj_not_viewable.npy", pcaobj)
-
-
-sys.exit(
-    f"""PCA successfully conducted with {NUM_PCS} Principle Components, removing {CROSS_VAL}% of data per round.\n
-Please see \"{PCA_OUTPUT}/dataframes.txt\" for the dataframes analysed by PCA.\n
-Please see \"{PCA_OUTPUT}/diagnostics.txt\" for the diagnostics sent to the terminal.\n
-Please see \"{PCA_OUTPUT}/pcaobj.txt\" for the elements of the PCA model.
-"""
-)
+    sys.exit(
+        f"""PCA successfully conducted with {NUM_PCS} Principle Components, removing {CROSS_VAL}% of data per round.\n
+    Please see \"{PCA_OUTPUT}/dataframes.txt\" for the dataframes analysed by PCA.\n
+    Please see \"{PCA_OUTPUT}/diagnostics.txt\" for the diagnostics sent to the terminal.\n
+    Please see \"{PCA_OUTPUT}/pcaobj.txt\" for the elements of the PCA model.
+    """
+    )
